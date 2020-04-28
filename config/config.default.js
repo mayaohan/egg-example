fa8870903af6961b7fee62d998077f4d9bafcb07
mayaohan <35213799+mayaohan@users.noreply.github.com>
2020-04-28 17:49:08 +0800

    Update config.default.js

--- config/config.default.js
+++ config/config.default.js
@@ -36,6 +36,21 @@ module.exports = appInfo =>{
         domainWhiteList:['.domain.com'],  // 安全白名单，以 . 开头
     };
 
+    config.uuint = {
+        id: 0, // 0-511
+        seed: 156015570 // a time
+    };
+
+    config.mysql = {
+        client: {
+          host: '127.0.0.1',
+          port: '3306',
+          user: 'root',
+          password: '123456',
+          database: 'test',
+        },
+    };
+
 
     config.robot = {
         ua:[
